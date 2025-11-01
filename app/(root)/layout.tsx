98859d6603caed9ec58dc621ee35371f74fe2bb9
MilitsaTsvetkova <militsa88@yahoo.com>
2025-11-01 12:28:50 +0200

    theming fonts background animation

--- app/(root)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>Root Layout {children}</div>;
-};
-
-export default Layout;
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Developer <br /> Events You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences - All in One Place!
+      </p>
+    </section>
+  );
+};
+
+export default Page;
