a18ffb6c34af9a61f6249733c8c148ce8341620b
MilitsaTsvetkova <militsa88@yahoo.com>
2025-11-08 14:48:33 +0200

    Book event form

---
+++ components/BookEvent.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+
+const BookEvent = () => {
+  const [email, setEmail] = React.useState("");
+  const [submitted, setSubmitted] = React.useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
+
+  return (
+    <div id="book-event">
+      {submitted ? (
+        <p className="text-sm">Thank you for signing up!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <button type="submit" className="button-submit">
+            Book Now
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default BookEvent;
