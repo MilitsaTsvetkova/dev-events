21bc47b8904516b9da64c035146420a736d0f620
MilitsaTsvetkova <militsa88@yahoo.com>
2025-11-08 15:33:09 +0200

    implemented caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import ExploreButton from "@/components/ExploreButton";
 import EventCard from "../components/EventCard";
 import { IEvent } from "../database";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
+  "use cache";
+  cacheLife("hours");
+
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
